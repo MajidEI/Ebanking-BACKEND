7aee7ab30b5fbef7dfedd92a07988de2bec01d39
MajidEi <abdelmajededdahbi@gmail.com>
2021-07-02 23:12:03 +0100

    finished

---
+++ src/main/java/ma/ensa/banki/entities/Rdv.java
@@ -0,0 +1,25 @@
+package ma.ensa.banki.entities;
+
+import lombok.Data;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter @Setter @ToString
+public class Rdv {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	@ManyToOne
+	Client client;
+	@ManyToOne
+	Agence agence;
+	@Column(nullable = false, columnDefinition = "DATE")
+	LocalDate dateRdv;
+
+	@Column
+	String service;
+}
